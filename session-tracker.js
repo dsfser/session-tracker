940b72e2062945ed20cb16dbd4d89f24e5ee21d5
dsfser <dsfser@hotmail.com>
2016-02-26 12:22:42 +0100

    initial commit

---
+++ session-tracker.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Eigenaar on 26-2-2016.
+ */
+/**
+ * Created by Patrick Falize on 12-2-2016.
+ */
+
+(function ( $ ) {
+
+    $.fn.sessionTracker = function(TimeoutSec) {
+
+        
+
+    };
+
+}( jQuery ));
